9ab79d2b4e1699b2064905c5caa8e4acb88e2770
Andrew Snodgrass <pointlander@gmail.com>
2024-11-17 12:22:05 -0600

    Added iterative model

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"bytes"
 	"embed"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"math"
@@ -103,7 +104,8 @@ func Dot(a, b, x, y []float64) (z float64) {
 	return z
 }
 
-func main() {
+// Random is random mode
+func Random() {
 	rng := rand.New(rand.NewSource(1))
 	data := Load()
 	u := make([]float64, Width)
@@ -203,5 +205,99 @@ func main() {
 		for _, item := range data {
 			fmt.Println(item.S, item.Label)
 		}
+	}
+}
+
+// Iterative is the iterative mode
+func Iterative() {
+	rng := rand.New(rand.NewSource(1))
+	data := Load()
+	u := make([]float64, Width)
+	for _, item := range data {
+		for i, v := range item.Measures {
+			u[i] += v
+		}
+	}
+	n := float64(len(data))
+	for i, v := range u {
+		u[i] = v / n
+	}
+	s := make([]float64, Width)
+	for _, item := range data {
+		for i, v := range item.Measures {
+			d := v - u[i]
+			s[i] += d * d
+		}
+	}
+	for i, v := range s {
+		s[i] = math.Sqrt(v / n)
+	}
+	length := len(data)
+	for i := 0; i < 50; i++ {
+		measures := make([]float64, Width)
+		for j := range measures {
+			measures[j] = s[j]*rng.NormFloat64() + u[j]
+		}
+		data = append(data, Fisher{
+			Measures: measures,
+			Label:    "Iris-fake",
+			Index:    length + i,
+		})
+	}
+	output := []Fisher{}
+	for e := 0; e < 199; e++ {
+		graph := pagerank.NewGraph()
+		for i, a := range data {
+			for j, b := range data {
+				x := make([]float64, Width)
+				y := make([]float64, Width)
+				aa := 0.0
+				for k, v := range a.Measures {
+					v += x[k]
+					aa += v * v
+				}
+				bb := 0.0
+				for k, v := range b.Measures {
+					v += y[k]
+					bb += v * v
+				}
+				weight := Dot(a.Measures, b.Measures, x, y)
+				graph.Link(uint32(i), uint32(j), math.Abs(weight)/(aa*bb))
+			}
+		}
+		r := make([]float64, len(data))
+		graph.Rank(1.0, 1e-6, func(node uint32, rank float64) {
+			r[node] = rank
+		})
+		for i := range data {
+			data[i].S = r[i]
+		}
+		sort.Slice(data, func(i, j int) bool {
+			return data[i].S > data[j].S
+		})
+		output = append(output, data[len(data)-1])
+		data = data[:len(data)-1]
+	}
+	for _, item := range output {
+		fmt.Println(item.Label, item.S)
+	}
+}
+
+var (
+	// FlagRandom is the random mode
+	FlagRandom = flag.Bool("random", false, "random mode")
+	// FlagIterative is the iterative mode
+	FlagIterative = flag.Bool("iterative", false, "iterative mode")
+)
+
+func main() {
+	flag.Parse()
+
+	if *FlagRandom {
+		Random()
+		return
+	} else if *FlagIterative {
+		Iterative()
+		return
 	}
 }
