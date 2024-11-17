9450c04daea0e53beb0693f0d53c111b45a39273
Andrew Snodgrass <pointlander@gmail.com>
2024-11-17 17:04:37 -0600

    Differential model

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ import (
 	"sort"
 	"strconv"
 
+	"github.com/pointlander/vino/kmeans"
+
 	"github.com/alixaxel/pagerank"
 )
 
@@ -283,11 +285,147 @@ func Iterative() {
 	}
 }
 
+// Differential is the differential mode
+func Differential() {
+	rng := rand.New(rand.NewSource(1))
+	data := Load()
+	u := make([]float64, Width)
+	for _, item := range data {
+		for i, v := range item.Measures {
+			u[i] += v
+		}
+	}
+	n := float64(len(data))
+	for i, v := range u {
+		u[i] = v / n
+	}
+	s := make([]float64, Width)
+	for _, item := range data {
+		for i, v := range item.Measures {
+			d := v - u[i]
+			s[i] += d * d
+		}
+	}
+	for i, v := range s {
+		s[i] = math.Sqrt(v / n)
+	}
+	length := len(data)
+	for i := 0; i < 50; i++ {
+		measures := make([]float64, Width)
+		for j := range measures {
+			measures[j] = s[j]*rng.NormFloat64() + u[j]
+		}
+		data = append(data, Fisher{
+			Measures: measures,
+			Label:    "Iris-fake",
+			Index:    length + i,
+		})
+	}
+	output := make([][]float64, 0, 200)
+	for e := 0; e < 200; e++ {
+		dgraph := pagerank.NewGraph()
+		for i, a := range data {
+			if e == i {
+				continue
+			}
+			for j, b := range data {
+				if e == j {
+					continue
+				}
+				x := make([]float64, Width)
+				y := make([]float64, Width)
+				aa := 0.0
+				for k, v := range a.Measures {
+					v += x[k]
+					aa += v * v
+				}
+				bb := 0.0
+				for k, v := range b.Measures {
+					v += y[k]
+					bb += v * v
+				}
+				weight := Dot(a.Measures, b.Measures, x, y)
+				dgraph.Link(uint32(i), uint32(j), math.Abs(weight)/(aa*bb))
+			}
+		}
+		dr := make([]float64, len(data))
+		dgraph.Rank(1.0, 1e-6, func(node uint32, rank float64) {
+			dr[node] = rank
+		})
+
+		graph := pagerank.NewGraph()
+		for i, a := range data {
+			for j, b := range data {
+				x := make([]float64, Width)
+				y := make([]float64, Width)
+				aa := 0.0
+				for k, v := range a.Measures {
+					v += x[k]
+					aa += v * v
+				}
+				bb := 0.0
+				for k, v := range b.Measures {
+					v += y[k]
+					bb += v * v
+				}
+				weight := Dot(a.Measures, b.Measures, x, y)
+				graph.Link(uint32(i), uint32(j), math.Abs(weight)/(aa*bb))
+			}
+		}
+		r := make([]float64, len(data))
+		graph.Rank(1.0, 1e-6, func(node uint32, rank float64) {
+			r[node] = rank
+		})
+		for i := range r {
+			r[i] = dr[i] - r[i]
+		}
+		cp := make([]Fisher, len(data))
+		copy(cp, data)
+		for i := range cp {
+			cp[i].S = r[i]
+		}
+		sort.Slice(cp, func(i, j int) bool {
+			return cp[i].S > cp[j].S
+		})
+		for i := range r {
+			r[i] = float64(cp[i].Index)
+		}
+		output = append(output, r)
+	}
+	meta := make([][]float64, len(output))
+	for i := range meta {
+		meta[i] = make([]float64, len(output))
+	}
+	for i := 0; i < 256; i++ {
+		clusters, _, err := kmeans.Kmeans(int64(i+1), output, 4, kmeans.SquaredEuclideanDistance, -1)
+		if err != nil {
+			panic(err)
+		}
+		for i := 0; i < len(output); i++ {
+			target := clusters[i]
+			for j, v := range clusters {
+				if v == target {
+					meta[i][j]++
+				}
+			}
+		}
+	}
+	clusters, _, err := kmeans.Kmeans(1, meta, 4, kmeans.SquaredEuclideanDistance, -1)
+	if err != nil {
+		panic(err)
+	}
+	for i, item := range data {
+		fmt.Println(clusters[i], item.Label)
+	}
+}
+
 var (
 	// FlagRandom is the random mode
 	FlagRandom = flag.Bool("random", false, "random mode")
 	// FlagIterative is the iterative mode
 	FlagIterative = flag.Bool("iterative", false, "iterative mode")
+	// FlagDifferential is the differential mode
+	FlagDifferential = flag.Bool("differential", false, "differential mode")
 )
 
 func main() {
@@ -298,6 +436,9 @@ func main() {
 		return
 	} else if *FlagIterative {
 		Iterative()
+		return
+	} else if *FlagDifferential {
+		Differential()
 		return
 	}
 }
