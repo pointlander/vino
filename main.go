b5f3fb9835c2fff5cc7e98d19dd824c483f6600a
Andrew Snodgrass <pointlander@gmail.com>
2024-11-18 13:14:03 -0600

    Copy neural network code from reason

--- main.go
+++ main.go
@@ -16,8 +16,14 @@ import (
 	"math/rand"
 	"sort"
 	"strconv"
+	"strings"
 
+	"github.com/pointlander/gradient/tf64"
 	"github.com/pointlander/vino/kmeans"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 
 	"github.com/alixaxel/pagerank"
 )
@@ -27,6 +33,26 @@ const (
 	Width = 4
 	// Factor is the gaussian factor
 	Factor = .1
+)
+
+const (
+	// S is the scaling factor for the softmax
+	S = 1.0 - 1e-300
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = .01
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
 )
 
 //go:embed iris.zip
@@ -386,4 +412,135 @@ func main() {
 		Differential()
 		return
 	}
+
+	data := Load()
+	rng := rand.New(rand.NewSource(1))
+	set := tf64.NewSet()
+	set.Add("w1", 4, 3)
+	set.Add("b1", 3)
+	set.Add("w2", 3, 4)
+	set.Add("b2", 4)
+
+	for i := range set.Weights {
+		w := set.Weights[i]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float64, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float64, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(float64(w.S[0]))
+		for i := 0; i < cap(w.X); i++ {
+			w.X = append(w.X, rng.NormFloat64()*factor)
+		}
+		w.States = make([][]float64, StateTotal)
+		for i := range w.States {
+			w.States[i] = make([]float64, len(w.X))
+		}
+	}
+
+	others := tf64.NewSet()
+	others.Add("input", 4)
+	others.Add("output", 4)
+
+	for i := range others.Weights {
+		w := others.Weights[i]
+		w.X = w.X[:cap(w.X)]
+	}
+
+	l1 := tf64.Sigmoid(tf64.Add(tf64.Mul(set.Get("w1"), others.Get("input")), set.Get("b1")))
+	l2 := tf64.Add(tf64.Mul(set.Get("w2"), l1), set.Get("b2"))
+	loss := tf64.Quadratic(l2, others.Get("output"))
+
+	points := make(plotter.XYs, 0, 8)
+	for i := 0; i < 33*len(data); i++ {
+		pow := func(x float64) float64 {
+			y := math.Pow(x, float64(i+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return y
+		}
+
+		others.Zero()
+		index := rng.Intn(len(data))
+		input := others.ByName["input"].X
+		for j := range input {
+			input[j] = data[index].Measures[j]
+		}
+		output := others.ByName["output"].X
+		for j := range output {
+			output[j] = data[index].Measures[j]
+		}
+
+		set.Zero()
+		cost := tf64.Gradient(loss).X[0]
+
+		norm := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := pow(B1), pow(B2)
+		if norm > 1 {
+			scaling := 1 / norm
+			for _, w := range set.Weights {
+				for l, d := range w.D {
+					g := d * scaling
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
+		} else {
+			for _, w := range set.Weights {
+				for l, d := range w.D {
+					g := d
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "epochs.png")
+	if err != nil {
+		panic(err)
+	}
+
 }
