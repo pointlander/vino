72ce7be2322f5cb346d9b14b07c50ffceea8c14f
Andrew Snodgrass <pointlander@gmail.com>
2024-11-19 15:21:30 -0600

    Improvements

--- main.go
+++ main.go
@@ -557,15 +557,17 @@ func main() {
 		index := rng.Intn(len(data))
 		network, min := 0, math.MaxFloat64
 		samples := make([][]float64, 0, 8)
+		inputs := make([][]float64, 0, 8)
 		for s := 0; s < 16; s++ {
 			transform := MakeRandomTransform(rng, Width, Width, Scale)
 			in := NewMatrix(Width, 1, data[index].Measures...)
 			in = transform.MulT(in)
+			inputs = append(inputs, in.Data)
 			sample := make([]float64, len(networks))
 			for n := range networks {
 				networks[n].Others.Zero()
 				copy(networks[n].Others.ByName["input"].X, in.Data)
-				copy(networks[n].Others.ByName["output"].X, in.Data)
+				copy(networks[n].Others.ByName["output"].X, data[index].Measures)
 				networks[n].Loss(func(a *tf64.V) bool {
 					sample[n] = a.X[0]
 					return true
@@ -598,42 +600,44 @@ func main() {
 			}
 		}
 
-		networks[network].Others.Zero()
-		copy(networks[network].Others.ByName["input"].X, data[index].Measures)
-		copy(networks[network].Others.ByName["output"].X, data[index].Measures)
+		for _, in := range inputs {
+			networks[network].Others.Zero()
+			copy(networks[network].Others.ByName["input"].X, in)
+			copy(networks[network].Others.ByName["output"].X, data[index].Measures)
 
-		networks[network].Set.Zero()
-		cost := tf64.Gradient(networks[network].Loss).X[0]
+			networks[network].Set.Zero()
+			cost := tf64.Gradient(networks[network].Loss).X[0]
 
-		norm := 0.0
-		for _, p := range networks[network].Set.Weights {
-			for _, d := range p.D {
-				norm += d * d
-			}
-		}
-		norm = math.Sqrt(norm)
-		b1, b2 := pow(B1), pow(B2)
-		scaling := 1.0
-		if norm > 1 {
-			scaling = 1 / norm
-		}
-		for _, w := range networks[network].Set.Weights {
-			for l, d := range w.D {
-				g := d * scaling
-				m := B1*w.States[StateM][l] + (1-B1)*g
-				v := B2*w.States[StateV][l] + (1-B2)*g*g
-				w.States[StateM][l] = m
-				w.States[StateV][l] = v
-				mhat := m / (1 - b1)
-				vhat := v / (1 - b2)
-				if vhat < 0 {
-					vhat = 0
+			norm := 0.0
+			for _, p := range networks[network].Set.Weights {
+				for _, d := range p.D {
+					norm += d * d
 				}
-				w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
 			}
-		}
+			norm = math.Sqrt(norm)
+			b1, b2 := pow(B1), pow(B2)
+			scaling := 1.0
+			if norm > 1 {
+				scaling = 1 / norm
+			}
+			for _, w := range networks[network].Set.Weights {
+				for l, d := range w.D {
+					g := d * scaling
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
 
-		points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
+			points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
+		}
 	}
 
 	histogram := [3][3]float64{}
@@ -648,7 +652,7 @@ func main() {
 			for n := range networks {
 				networks[n].Others.Zero()
 				copy(networks[n].Others.ByName["input"].X, in.Data)
-				copy(networks[n].Others.ByName["output"].X, in.Data)
+				copy(networks[n].Others.ByName["output"].X, data[index].Measures)
 				networks[n].Loss(func(a *tf64.V) bool {
 					sample[n] = a.X[0]
 					return true
